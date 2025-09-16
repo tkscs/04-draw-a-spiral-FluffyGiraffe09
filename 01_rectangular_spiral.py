052c16bd7ebe10dad7c739bda45ddd850487b83b
FluffyGiraffe09 <oroy2028@kehillahstudent.org>
2025-09-16 10:26:49 -0700

    spiral levels 1-3

--- 01_rectangular_spiral.py
+++ 01_rectangular_spiral.py
@@ -5,7 +5,13 @@ Make a rectangular spiral (see the README.md for an example)
 """
 
 ### YOUR CODE STARTS HERE
+turtle.right(90)
 
+for i in range(10, 60, 10):
+    turtle.forward(i)
+    turtle.left(90)
+    turtle.forward(i)
+    turtle.left(90)
 
 ### YOUR CODE ENDS HERE
 
